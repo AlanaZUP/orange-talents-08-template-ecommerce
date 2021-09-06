7cd8805624d0e2bf54a711c547a6db6abe022a92
AlanaZUP <alana.reis@zup.com.br>
2021-09-06 16:32:28 -0300

    Teste para validação de campo único

--- src/integrationTest/java/com/orangetalents/mercadolivre/cadastro/CadastrarUsuarioTest.java
+++ src/integrationTest/java/com/orangetalents/mercadolivre/cadastro/CadastrarUsuarioTest.java
@@ -10,6 +10,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.google.gson.Gson;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,6 +24,9 @@ public class CadastrarUsuarioTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private EntityManager manager;
 
     Gson gson = new Gson();
 
@@ -40,6 +47,7 @@ public class CadastrarUsuarioTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[0].campo").value("login"));
     }
 
+
     @Test
     public void retornaBadRequestQuandoRecebeSenhaMenorQue6() throws Exception {
         UsuarioRequest usuarioRequest = new UsuarioRequest("login@login.com", "12345");
@@ -51,25 +59,25 @@ public class CadastrarUsuarioTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[0].campo").value("senha"));
     }
 
-    @Test
-    public void retornaBadRequestQuandoRecebeLoginESenhaInvalidos() throws Exception {
-        UsuarioRequest usuarioRequest = new UsuarioRequest("loginErrado", "12345");
-        mockMvc.perform(post("/cadastro")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(gson.toJson(usuarioRequest)))
-                .andExpect(status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].campo").value("login"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].campo").value("senha"));
-    }
 
     @Test
     public void retornaCreatedQuandoRecebeDadosCorretamente() throws Exception {
-        UsuarioRequest usuarioRequest = new UsuarioRequest("login@login.com", "123456");
+        UsuarioRequest usuarioRequest = new UsuarioRequest("login@teste.com", "123456");
         mockMvc.perform(post("/cadastro")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(gson.toJson(usuarioRequest)))
                 .andExpect(status().isCreated());
     }
+    @Test
+    public void retornaBadRequestQuandoRecebeLoginDuplicado() throws Exception {
+        UsuarioRequest usuarioRequest = new UsuarioRequest("login@login.com", "123456");
+        mockMvc.perform(post("/cadastro")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(usuarioRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].campo").value("login"));
+    }
+
 }
