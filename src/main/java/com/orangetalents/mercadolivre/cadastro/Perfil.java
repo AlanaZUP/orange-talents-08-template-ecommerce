ae62a6054ccdc205728ef7a02d3edb18890cd507
AlanaZUP <alana.reis@zup.com.br>
2021-09-08 15:30:22 -0300

    Entidade que representará os perfis da aplicação no Banco de Dados

--- src/main/java/com/orangetalents/mercadolivre/cadastro/Perfil.java
+++ src/main/java/com/orangetalents/mercadolivre/cadastro/Perfil.java
@@ -1,2 +1,37 @@
-package com.orangetalents.mercadolivre.cadastro;public class Perfil {
+package com.orangetalents.mercadolivre.cadastro;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank @Column(unique = true)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String getAuthority() {
+        return nome;
+    }
 }
