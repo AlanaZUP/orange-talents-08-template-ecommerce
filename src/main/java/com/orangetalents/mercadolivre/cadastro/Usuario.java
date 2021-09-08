40cebe93fd917dcc4694bca1617e44f9c1391ae8
AlanaZUP <alana.reis@zup.com.br>
2021-09-08 15:29:53 -0300

    Indicando classe usuário como base da autenticação

--- src/main/java/com/orangetalents/mercadolivre/cadastro/Usuario.java
+++ src/main/java/com/orangetalents/mercadolivre/cadastro/Usuario.java
@@ -1,14 +1,17 @@
 package com.orangetalents.mercadolivre.cadastro;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import javax.validation.constraints.*;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,9 +25,47 @@ public class Usuario {
     @PastOrPresent
     private LocalDateTime dataCadastro;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Perfil> perfis = new ArrayList<>();
+
     public Usuario(String login, SenhaLimpa senha){
         this.login = login;
         this.senha = senha.toHash();
         this.dataCadastro = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return   this.perfis;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
