00448f9fed31489b25759dd86143d402324a418a
AlanaZUP <alana.reis@zup.com.br>
2021-09-08 15:55:14 -0300

    Indicando que a autorizacao será através do token

--- src/main/java/com/orangetalents/mercadolivre/comms/security/SecurityConfiguration.java
+++ src/main/java/com/orangetalents/mercadolivre/comms/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -22,8 +23,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/categorias").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers(HttpMethod.POST,"/cadastro").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 
