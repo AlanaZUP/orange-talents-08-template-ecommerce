41bf6b31c0f17682bb41aa663398923917760b01
AlanaZUP <alana.reis@zup.com.br>
2021-09-08 15:29:14 -0300

    Security Configurations iniciais

--- src/main/java/com/orangetalents/mercadolivre/comms/security/SecurityConfiguration.java
+++ src/main/java/com/orangetalents/mercadolivre/comms/security/SecurityConfiguration.java
@@ -1,7 +1,30 @@
-package com.orangetalents.mercadolivre.comms.seguranca;
+package com.orangetalents.mercadolivre.comms.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    // Configurações de Autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    // Configurações de Autorizações
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/categorias").permitAll()
+                .anyRequest().authenticated();
+    }
+
+
 }
