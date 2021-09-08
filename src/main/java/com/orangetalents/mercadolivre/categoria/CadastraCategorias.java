f9ce828c606eb152038e2f7cd7290b62c071359d
AlanaZUP <alana.reis@zup.com.br>
2021-09-08 14:34:19 -0300

    Classe Controller Cadastra Categoria

--- src/main/java/com/orangetalents/mercadolivre/categoria/CadastraCategorias.java
+++ src/main/java/com/orangetalents/mercadolivre/categoria/CadastraCategorias.java
@@ -1,17 +1,23 @@
-package com.orangetalents.mercadolivre.teste;
+package com.orangetalents.mercadolivre.categoria;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/teste")
+@RequestMapping("/categorias")
 public class CadastraCategorias {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void teste(@RequestBody @Valid CategoriaRequest teste){
-
+    public CategoriaResponse cadastrar(@RequestBody @Valid CategoriaRequest categoriaRequest){
+        Categoria categoria = categoriaRequest.toModel(categoriaRepository);
+        categoria = categoriaRepository.save(categoria);
+        return new CategoriaResponse(categoria);
     }
 }
