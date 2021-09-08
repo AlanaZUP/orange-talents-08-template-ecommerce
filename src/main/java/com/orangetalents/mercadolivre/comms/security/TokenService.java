a49df11699a9fe5aaf6588b2f1ee9c81a87e7d45
AlanaZUP <alana.reis@zup.com.br>
2021-09-08 19:30:16 -0300

    Configuração do Token

--- src/main/java/com/orangetalents/mercadolivre/comms/security/TokenService.java
+++ src/main/java/com/orangetalents/mercadolivre/comms/security/TokenService.java
@@ -1,2 +1,48 @@
-package com.orangetalents.mercadolivre.comms.security;public class TokenService {
+package com.orangetalents.mercadolivre.comms.security;
+
+import com.orangetalents.mercadolivre.usuario.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${mercado-livre.jwt.expiration}")
+    private String expiration;
+
+    @Value("${mercado-livre.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authenticate) {
+        Usuario logado = (Usuario) authenticate.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+        return Jwts.builder()
+                .setIssuer("API forum Orange Talents - Mercado Livre")
+                .setSubject(logado.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public boolean isTokenValido(String token) {
+        try{
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public Long getIdUsuario(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
+    }
 }
