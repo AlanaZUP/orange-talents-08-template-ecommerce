983680dbaa2e2286bffe0344acfc6f946f5a390c
AlanaZUP <alana.reis@zup.com.br>
2021-09-09 12:19:12 -0300

    DTO Response Produto

--- src/main/java/com/orangetalents/mercadolivre/produto/ProdutoResponse.java
+++ src/main/java/com/orangetalents/mercadolivre/produto/ProdutoResponse.java
@@ -1,2 +1,83 @@
-package com.orangetalents.mercadolivre.produto;public class ProdutoResponse {
+package com.orangetalents.mercadolivre.produto;
+
+import com.orangetalents.mercadolivre.categoria.Categoria;
+import com.orangetalents.mercadolivre.produto.caracteristica.Caracteristica;
+import com.orangetalents.mercadolivre.usuario.Usuario;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ProdutoResponse {
+
+    private Long id;
+    private String nome;
+    private BigDecimal valor;
+    private Integer quantidade;
+    private String descricao;
+    private LocalDateTime instanteCadastro;
+    private Usuario usuario;
+    private Categoria categoria;
+    private List<Caracteristica> caracteristicas;
+
+    @Deprecated
+    public ProdutoResponse() {
+    }
+
+    public ProdutoResponse(Produto produto) {
+        this.id = produto.getId();
+        this.nome = produto.getNome();
+        this.valor = produto.getValor();
+        this.quantidade = produto.getQuantidade();
+        this.descricao = produto.getDescricao();
+        this.instanteCadastro = produto.getInstanteCadastro();
+        this.usuario = produto.getUsuario();
+        this.categoria = produto.getCategoria();
+        this.caracteristicas = produto.getCaracteristicas();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getInstanteCadastro() {
+        return instanteCadastro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public List<Caracteristica> getCaracteristicas() {
+        return caracteristicas;
+    }
 }
