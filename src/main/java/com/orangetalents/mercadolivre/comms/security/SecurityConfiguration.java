783c7a94e4d5629778a8d60462d51cab59562daf
AlanaZUP <alana.reis@zup.com.br>
2021-09-12 18:56:51 -0300

    Configurando permição de portas

--- src/main/java/com/orangetalents/mercadolivre/comms/security/SecurityConfiguration.java
+++ src/main/java/com/orangetalents/mercadolivre/comms/security/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/auth").permitAll()
+//                .antMatchers(HttpMethod.POST, "/usuarios").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
