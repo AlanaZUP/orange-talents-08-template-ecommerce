0265804740f2130d6981e3a984563393a281a6e5
AlanaZUP <alana.reis@zup.com.br>
2021-09-12 21:07:45 -0300

    Enviador de emails

--- src/main/java/com/orangetalents/mercadolivre/comms/utils/EnviaEmals.java
+++ src/main/java/com/orangetalents/mercadolivre/comms/utils/EnviaEmals.java
@@ -1,2 +1,6 @@
-package com.orangetalents.mercadolivre.comms.utils;public interface EnviaEmals {
+package com.orangetalents.mercadolivre.comms.utils;
+
+public interface EnviaEmals {
+
+    void envia(String remetente, String destinatário, String titulo, String mensagem);
 }
--- src/main/java/com/orangetalents/mercadolivre/comms/utils/EnviadorEmails.java
+++ src/main/java/com/orangetalents/mercadolivre/comms/utils/EnviadorEmails.java
@@ -1,2 +1,16 @@
-package com.orangetalents.mercadolivre.comms.utils;public class EnviadorEmails {
+package com.orangetalents.mercadolivre.comms.utils;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class EnviadorEmails implements EnviaEmals{
+    @Override
+    public void envia(String remetente, String destinatário, String titulo, String mensagem) {
+        System.out.println(remetente);
+        System.out.println(destinatário);
+        System.out.println(titulo);
+        System.out.println(mensagem);
+    }
 }
